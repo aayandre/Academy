ef63568f9696adc8bdcd5571710d1a4a5164356b
andre.ayamamoto <andre.ayamamoto@CAS576145W1021.senacsp.edu.br>
2017-08-10 21:55:04 -0300

    começo

--- ExerciciosFuncao/src/exerciciosfuncao/Ex01.java
+++ ExerciciosFuncao/src/exerciciosfuncao/Ex01.java
@@ -5,10 +5,29 @@
  */
 package exerciciosfuncao;
 
+import java.util.Scanner;
+
 /**
  *
  * @author andre.ayamamoto
  */
 public class Ex01 {
+
+    public static void main(String[] args) {
+
+    }
+
+    static Scanner ler = new Scanner(System.in);
     
+    static boolean lerEntrada(int numero) {
+        
+        
+    }
+    
+    static String parOuImpar(boolean resultado) {
+        
+        if(resultado)
+        
+    }
+
 }
