4969e33ccfceef12dfc1c25dbd5e818b0e4a8b17
aayandre <aayandre@github.com>
2018-04-07 11:09:46 -0300

    Inicio ado2

---
+++ POO/POO_ADO2/src/model/Product.java
@@ -0,0 +1,19 @@
+/*
+ * André de Amorim Yamamoto
+ * Github: aayandre
+ * Senac
+ */
+package model;
+
+/**
+ *
+ * @author mesa
+ */
+public class Product {
+    
+    //Propriedades
+    private int id;
+    private String name;
+    
+    
+}
